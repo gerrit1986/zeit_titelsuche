335b4a34d12efcfcb3c45d62bf09cd6377bf1854
Gerrit Holz <gerrit1986@gmail.com>
2013-04-12 00:23:33 +0200

    dynamic list, cleaned up

--- search/api.py
+++ search/api.py
@@ -1,53 +1,33 @@
 # coding: utf-8
-# from datetime import datetime
-import json
+import datetime
 import requests
-from django.contrib import messages
-
 
 def buildNewsItems(query):
     """Returns news_items: a list containing tuples with
     0: title
     1: subtile
     2: href
-    3: release_date
+    3: pub_date
     """
     title_query = 'title:' + unicode(query)
     params = {
         'q' : title_query,
-        'limit' : '3',
+        'limit' : '100',
         'fields' : 'title,subtitle,href,release_date',
     }
     url = 'http://api.zeit.de/content?'
-    headers = {'X-Authorization': 'YOUR API KEY'}
+    headers = {'X-Authorization': 'API_KEY'}
     request = requests.get(url, params=params, headers=headers)
     json = request.json()
     news_items = []
     try:
         for result in json['matches']:
-            news_items.append((result['title'], result['subtitle'], result['href'], result['release_date']))
+            pub_date = result['release_date']
+            pub_date = pub_date[0:10] + ' ' + pub_date[11:-1]
+            pub_date = datetime.datetime.strptime(pub_date, '%Y-%m-%d %H:%M:%S')
+            news_items.append((result['title'], result['subtitle'], result['href'], pub_date))
     except KeyError:
         pass
     return news_items
 
-
-# What happens here? Try below to check:
-# news = buildNewsItems('Merkel')
-
-# print all results to the console
-# try:
-#     for result in json['matches']:
-#         print 'Titel:', result['title']
-#         print 'Untertitel:', result['subtitle']
-#         print 'Link:', result['uri']
-#         print '--------'
-# except KeyError:
-#     pass
-
-    # some attempts to better time formatting
-    # time = news[0][3]
-    # print time
-    # date = time[0:10]
-    # clock = time[11:-1]
-    # time = datetime.strptime(date + ' ' + clock, "%Y-%m-%d %H:%M:%S")
-    # print time
+print buildNewsItems('Merkel')
--- search/views.py
+++ search/views.py
@@ -1,12 +1,11 @@
 # coding: utf-8
 import datetime
-from django.contrib import messages
 from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
 from api import buildNewsItems
 
-
+# homepage, 3 hardcoded items, old version
 def home(home):
     now = datetime.datetime.now()
     template = get_template('template.html')
@@ -21,26 +20,18 @@ def home(home):
                                     'news_link3' : news_items[2][2]}))
     return HttpResponse(html)
 
-
+# query, length depends on number of results
 def query(request, query):
     now = datetime.datetime.now()
     template = get_template('template.html')
     query = unicode(query)
     news_items = buildNewsItems(query)
     try:
-        dynamic_content = template.render(Context({'current_date': now,
+        content = template.render(Context({'current_date': now,
                                         'query' : query,
-                                        'news_title1' : news_items[0][0],
-                                        'news_title2' : news_items[1][0],
-                                        'news_title3' : news_items[2][0],
-                                        'news_link1' : news_items[0][2],
-                                        'news_link2' : news_items[1][2],
-                                        'news_link3' : news_items[2][2],
-                                        'release_date1': news_items[0][3],
-                                        'release_date2': news_items[1][3],
-                                        'release_date3': news_items[2][3]}))
+                                        'news_items': news_items,}))
     except IndexError:
         # return messages.add_message(request, messages.INFO, 'Zu wenige Ergebnisse!')
         return HttpResponse("Ein Fehler ist aufgetreten. Für den Term „" + str(query) + "“ gibt es zu wenige "
                                                                                          "Ergebnisse.")
-    return HttpResponse(dynamic_content)
+    return HttpResponse(content)
